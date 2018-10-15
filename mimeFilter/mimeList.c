4f4e4f45da392b86387e76d50db921e39624ef1e
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-10-15 15:04:34 -0300

    progress with mimeList

---
+++ mimeFilter/mimeList.c
@@ -0,0 +1,135 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "mimeList.h"
+
+#define WILDCARD "*"
+
+
+struct List* create_list(void){
+
+	struct List* list = malloc(sizeof(*List));
+	if(list != NULL){
+        memset(list, 0, sizeof(*list)); // copia el caracter 0 a los primeros sizeof(*list) caracteres de list
+	}
+	return list;
+}
+
+int add_new(char* type, char* subtype, List* list){
+
+	if(list == null){
+		return -1; //error
+	}
+
+	if(list->first != null){
+
+		bool typeExists = false;
+		bool subtypeExists = false;
+
+		struct type_node*  tpnode = search_for_type(list,type,&typeExists);
+
+		//FALTA VER QUE PASA SI subtype es *
+
+		if(typeExists){
+
+				struct subtype_node* stpnode = search_for_subtype(tpnode->subtypes, subtype, &subtypeExists);
+
+				if(subtypeExists){
+					return -1;
+				}else{
+					//agrego nuevo subtipo al nodo tipo
+					stpnode->next = create_new_subtype(subtype);
+					return 0;
+				}
+		}else{
+				//agrego un nuevo tipo
+				tpnode->next = create_new_type(type);
+				tpnode->next->subtypes = create_new_subtype(subtype);
+				return 0;
+		}
+				
+
+	}else{
+		//tengo que inicializar 
+
+			list->first = create_new_type(type);
+			list->first->subtypes = create_new_subtype(subtype);
+			//FALTA VER QUE PASA SI subtype es *				
+			return 0;
+	}
+}
+
+
+struct type_node* search_for_type(struct List* list, char* type, bool* typeExists){
+	struct type_node* current = list->first;
+	if(strcmp(current->name,type) == 0){
+		*typeExists = true;
+		return current;
+	}
+	while(current->next != NULL){
+		current = current->next;
+		if(strcmp(current->name,type) == 0){
+			typeExists = true;
+			return current;
+		}
+	}
+	//sino lo encontre retorno el ultimo
+	return current;
+}
+
+struct subtype_node* search_for_subtype(struct subtype_node* current, char* subtype, bool* subtypeExists){
+	//FALTA VER QUE PASA SI subtype es *
+	if(strcmp(current->name,subtype) == 0){
+		*subtypeExists = true;
+		return current;
+	}
+	while(current->next != NULL){
+		current = current->next;
+		if(strcmp(current->name,subtype) == 0){
+			*subtypeExists = true;
+			return current;
+		}
+	}
+	return current;
+}
+
+struct type_node* create_new_type(char* name){
+
+	struct type_node* node = malloc(sizeof(*node));
+
+	if(node != NULL){
+		memset(node,0,sizeof(*node)); 
+		
+
+		//node->parser = parser_init(parser_no_classes(), def);
+		//node->def	= def;
+		node->next = NULL;
+		node->subtypes = NULL;
+		node->name = name;
+		//node->event = NULL;
+		//node->wildcard = false;		
+	}
+	return node;
+}
+
+struct subtype_node* create_new_subtype(char* name){
+
+	struct subtype_node* node = malloc(sizeof(*node));
+
+	if(node != NULL){
+		memset(node,0,sizeof(*node)); 
+		
+
+		//node->parser = parser_init(parser_no_classes(), def);
+		//node->def	= def;
+		node->next = NULL;
+		node->name = name;
+		//node->event = NULL;
+		//node->wildcard = false;		
+	}
+	return node;
+}
+
+//FALTA remover subtypes de un type_node, remover un nodo, destruir nodo
+// destrui y resetear parser
+// el tema de las * en todos lados
---
+++ mimeFilter/mimeList.h
@@ -0,0 +1,45 @@
+#ifndef MIMELIST_H_
+#define MIMELIST_H_
+
+
+struct type_node{
+	//struct parser* parser;
+	//struct parser_definition *def;
+	struct type_node *next;
+	struct subtype_node *subtypes;
+	const char* name;
+	//const struct parser_event* event;
+	//bool wildcard;
+};
+
+struct subtype_node{
+	//struct parser* parser;
+	//struct parser_definition *def;
+	struct subtype_node *next;
+	const char* name;
+	//const struct parser_event* event;
+	//bool wildcard;
+};
+
+struct List{
+	struct type_node* first;
+};
+
+struct mime{
+	char* type;
+	char* subtype;
+};
+
+struct List* create_list(void);
+
+int add_new(char* type, char* subtype, List* list);
+
+struct type_node* search_for_type(struct List* list, char* type, bool* typeExists);
+
+struct subtype_node* search_for_subtype(struct subtype_node* current, char* subtype, bool* subtypeExists);
+
+struct type_node* create_new_type(char* name);
+
+struct subtype_node* create_new_subtype(char* name);
+
+#endif
