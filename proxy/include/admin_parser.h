4012f76d1f5a599ec66f274c08953d3f6d8df0d4
Rocío Della Sala <rdella@itba.edu.ar>
2018-10-25 23:36:41 -0300

    fix

--- proxy/include/admin_parser.h
+++ proxy/include/admin_parser.h
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-//
-// Created by rocio on 24/10/18.
-//
-
-#ifndef PROXYPOP3_ADMIN_PARSER_H
-#define PROXYPOP3_ADMIN_PARSER_H
-
-=======
 #ifndef PROXYPOP3_ADMIN_PARSER_H
 #define PROXYPOP3_ADMIN_PARSER_H
 
@@ -19,5 +10,4 @@ int parse_admin_request(struct admin *);
 void parse_admin_response(struct admin *);
 void send_response_without_data(file_descriptor, unsigned char);
 
->>>>>>> f63ebf25f7c99555cb02535cd0d41dc4fef5c2bc
 #endif //PROXYPOP3_ADMIN_PARSER_H
--- proxy/include/metrics.h
+++ proxy/include/metrics.h
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-//
-// Created by rocio on 24/10/18.
-//
-
-#ifndef PROXYPOP3_METRICS_H
-#define PROXYPOP3_METRICS_H
-
-=======
 #ifndef PROXYPOP3_METRICS_H
 #define PROXYPOP3_METRICS_H
 
@@ -21,5 +12,4 @@ typedef struct metrics * metrics;
 extern metrics program_metrics;
 
 
->>>>>>> f63ebf25f7c99555cb02535cd0d41dc4fef5c2bc
 #endif //PROXYPOP3_METRICS_H
