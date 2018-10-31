3c56f64c6aaa4f5a34ef3573d24b13410303cc76
Florencia Giorgi <mgiorgi@itba.edu.ar>
2018-10-31 02:36:49 -0300

    minor fix

---
+++ utils.h
@@ -0,0 +1,24 @@
+#ifndef PROXYPOP3_UTILS_H
+#define PROXYPOP3_UTILS_H
+
+#include <stdbool.h>
+
+#define SOCKADDR_TO_HUMAN_MIN (INET6_ADDRSTRLEN + 5 + 1)
+#define VERSION 0X01
+#define MAX_ADMIN_DATA 100
+#define MAX_ADMIN_BUFFER 9 + MAX_ADMIN_DATA /* 3 version (uchar), 3 cmd (uchar), 3 length y 100 max data (definido por nosotros) */
+
+typedef int file_descriptor;
+
+/* Describe de forma humana un sockaddr */
+const char * sockaddr_to_human(char *, size_t, const struct sockaddr *);
+
+void print_connection_status(const char *, struct sockaddr_storage, file_descriptor);
+
+int get_int_len(int);
+
+bool compare_strings(const char *, const char *);
+
+char * append_cmd(char *, char *, const char * cmd);
+
+#endif //PROXYPOP3_UTILS_H
